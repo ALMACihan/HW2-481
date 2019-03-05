dcd48a9a477987e86875223a72cdc8a630367bde
ALMACihan <cihan.alma96@gmail.com>
2019-03-05 21:50:37 +0300

    rename the Tests

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -7,48 +7,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
-  Util c;
+	Util c;
 
-  @Before
-  public void setUp() { c = new Util(); }
+	@Before
+	public void setUp() { c = new Util(); }
 
-  @Test
-  public void example() { assertTrue(true); }
-  
-
-
-  	@Test
-  	public void testOnlyOne() {
-  		c.compute(1);
-
-  	}
 	@Test
-  	public void testAllZeros() {
+	public void example() { assertTrue(true); }
+
+
+	@Test
+	public void testOnlyOne() {
+		c.compute(1);
+
+	}
+	@Test
+	public void testAllZeros() {
 		try {
-  		c.compute(0,0,0,0,0);
+			c.compute(0,0,0,0,0);
 		}catch(Exception e) {
-			
+
 		}
-  	}
-	
-  	@Test
-  	public void testOddNumber() {
-  		c.compute(1,2,3);
+	}
 
-  	}
-  	@Test
-  	public void testEvenNumber() {
-  		c.compute(1,2,3,4);
-
-  	}
-
-	
 	@Test
-  	public void testReturnFalse() {
-  		c.compute(5,7,11,13,17);
+	public void testOddNumber() {
+		c.compute(1,2,3);
 
-  	}
-  
+	}
+	@Test
+	public void testEvenNumber() {
+		c.compute(1,2,3,4);
+	}
+
+	@Test
+	public void testReturnFalse() {
+		c.compute(5,7,11,13,17);
+
+	}
+
 }
 
 
